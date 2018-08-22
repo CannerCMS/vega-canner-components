62f4d15877c1e3d7b364a8cf76bba10415f2111c
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-08-22 19:29:21 +0800

    Create docs

---
+++ docs/demos/ExamplePrimitiveValueHoc.js
@@ -0,0 +1,47 @@
+// @flow
+import * as React from 'react';
+import Inspector from 'react-inspector';
+import type RefId from 'canner-ref-id';
+import {set, update} from 'lodash';
+
+type PrimitiveValue = string | boolean | number | Object | Array<any>;
+
+export default (defaultValue: PrimitiveValue, rootValue: PrimitiveValue) => (ConfigOrComposedComponent: React.Element<*>) => {
+  class ExamplePrimitiveValueWrapper extends ConfigOrComposedComponent {
+    constructor(props: any) {
+      super(props);
+
+      this.state = {
+        log: [],
+        value: defaultValue,
+        rootValue: defaultValue || rootValue
+      };
+    }
+  
+    render() {
+      const {log, value} = this.state;
+
+      return (
+        <div>
+          <ConfigOrComposedComponent
+            {...this.props}
+            value={value}
+            />
+          {log.length ?
+          <div>
+            <h4>onChange log:</h4>
+            <div style={{border: '1px solid #CCC', padding: '10px'}}>
+              {log.map((item, i) => {
+                return (
+                  <Inspector data={item} key={i}/>
+                );
+              })}
+            </div>
+          </div> : null}
+        </div>
+      );
+    }
+  }
+
+  return ExamplePrimitiveValueWrapper;
+};
