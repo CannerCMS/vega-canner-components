3ded7b5d805a4435e71dca8858d78fb2a7eb3695
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2018-08-23 13:37:38 +0800

    Add vega-chart-pie

---
+++ packages/vega-chart-pie/src/index.js
@@ -0,0 +1,19 @@
+// @flow
+import React from "react";
+import Vega from 'react-vega';
+
+import createSpec from './createSpec';
+
+// types
+import type {ChartDefaultProps} from 'types/ChartDefaultProps';
+import type {UIParams} from './types';
+
+type Props = ChartDefaultProps & {
+  uiParams: UIParams
+};
+
+const PieChart = ({ value, uiParams }: Props) => ((
+  <Vega spec={createSpec(uiParams)} data={{table: value}} />
+));
+
+export default PieChart;
