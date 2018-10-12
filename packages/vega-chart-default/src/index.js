4d365eb7422cd840b72ca084d1e700555e9daf34
chilijung <chilijung@gmail.com>
2018-10-12 19:21:13 +0800

    move spec to props

--- packages/vega-chart-default/src/index.js
+++ packages/vega-chart-default/src/index.js
@@ -1,19 +1,17 @@
 // @flow
 import React from "react";
-import Vega from 'react-vega';
+import Vega from "react-vega";
 
 // types
-import type {ChartDefaultProps} from 'types/ChartDefaultProps';
-import type {UIParams} from './types';
+import type { ChartDefaultProps } from "types/ChartDefaultProps";
+import type { UIParams } from "./types";
 
 type Props = ChartDefaultProps & {
   uiParams: UIParams
 };
 
-const VegaChart = ({ value, uiParams }: Props) => {
-  return (
-    <Vega spec={uiParams.spec} data={value} />
-  )
+const VegaChart = ({ value, spec }: Props) => {
+  return <Vega spec={spec} data={value} />;
 };
 
 export default VegaChart;
