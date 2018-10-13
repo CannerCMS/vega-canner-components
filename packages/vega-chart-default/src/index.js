35bbce58d9d2c3ab4ad9965d068b8f60fa7d3731
Li Jung Chi <lijungchi@Lis-MacBook-Pro.local>
2018-10-13 21:01:46 +0800

    add auto resize in customize chart

--- packages/vega-chart-default/src/index.js
+++ packages/vega-chart-default/src/index.js
@@ -4,14 +4,15 @@ import Vega from "react-vega";
 
 // types
 import type { ChartDefaultProps } from "types/ChartDefaultProps";
+import { withAutoContainerSize } from "@canner/vega-charts-utils";
 import type { UIParams } from "./types";
 
 type Props = ChartDefaultProps & {
   uiParams: UIParams
 };
 
-const VegaChart = ({ value, spec }: Props) => {
-  return <Vega spec={spec} data={value} />;
+const VegaChart = ({ value, spec, getVegaRef }: Props) => {
+  return <Vega ref={getVegaRef} spec={spec} data={value} />;
 };
 
-export default VegaChart;
+export default withAutoContainerSize(VegaChart);
